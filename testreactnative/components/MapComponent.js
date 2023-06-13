cee468af6ef7d7642ac69e9cae4126033f0ed9d1
Dávid Borsodi <smarton0614@gmail.com>
2023-06-13 13:16:22 +0200

    push

--- testreactnative/components/MapComponent.js
+++ testreactnative/components/MapComponent.js
@@ -9,30 +9,35 @@ export default function MapComponent() {
   const vineyards = [
     {
       name: "Esterházy Vineyard",
+      image: 'https://www.hallwines.com/media/wysiwyg/hall/vineyards/Vineyard_BBS_1515_768px.jpg',
       latitude: 47.677493,
       longitude: 16.574725,
       id: 1,
     },
     {
       name: "Taschner Vineyard",
+      image: 'https://images.unsplash.com/photo-1563514227147-6d2ff665a6a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmluZXlhcmR8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
       latitude: 47.680965,
       longitude: 16.595614,
       id: 2,
     },
     {
       name: "Pfneiszl Vineyard",
+      image: 'https://www.hallwines.com/media/wysiwyg/hall/vineyards/Vineyard_BBS_1515_768px.jpg',
       latitude: 47.698976,
       longitude: 16.551085,
       id: 3,
     },
     {
       name: "Söptei Vineyard",
+      image: 'https://i.insider.com/615fd15a5ae4fe0018a708e6?width=1136&format=jpeg',
       latitude: 47.676683,
       longitude: 16.579596,
       id: 4,
     },
     {
       name: "Liszkay Vineyard",
+      image: 'https://images.squarespace-cdn.com/content/v1/58ae0e6c3e00be7a0f66a26b/1658164270174-VJ4NPBPNQ6WGRCIPVY15/IMG_20191101_140942142_HDR.jpg',
       latitude: 47.665987,
       longitude: 16.561179,
       id: 5,
@@ -153,25 +158,31 @@ export default function MapComponent() {
         })}
       </MapView>
       <View style={styles.carousel}>
-      <TouchableOpacity style={styles.button} onPress={handleResetLocation}>
-        <Text style={styles.buttonText}>
-          <MaterialIcons name="my-location" size={38} color="#FFF" />
-        </Text>
-      </TouchableOpacity>
-        <Swiper style={styles.swiperContainer}>
-          <View style={styles.slide}>
-            <Text>Slide 1</Text>
-          </View>
-          <View style={styles.slide}>
-            <Text>Slide 2</Text>
-          </View>
-          <View style={styles.slide}>
-            <Text>Slide 3</Text>
-          </View>
+        <TouchableOpacity style={styles.button} onPress={handleResetLocation}>
+          <Text style={styles.buttonText}>
+            <MaterialIcons name="my-location" size={38} color="#FFF" />
+          </Text>
+        </TouchableOpacity>
+        <Swiper style={styles.swiperContainer} showsPagination={false}>
+          {vineyards.map((vineyard) => {
+            return (
+              <View style={{display:'flex',justifyContent:'center'}}>
+                
+                <Image
+                  style={styles.image}
+                  source={{ uri: vineyard.image }}
+                />
+                </View>
+                
+              
+            )
+          })}
+
+
         </Swiper>
 
       </View>
-      
+
     </View>
   );
 }
@@ -185,22 +196,35 @@ const styles = StyleSheet.create({
   },
   carousel: {
     position: "absolute",
-    height:'25%',
-    bottom: 15,
-    right:15,
-    left:15,
-    borderRadius:10,
-    backgroundColor:'white'
+    height: '25%',
+    bottom: 30,
+    right: 15,
+    left: 15,
+    borderRadius: 10,
+    backgroundColor: 'white'
+  },
+  swiperContainer:{
+   
   },
   button: {
     position: "absolute",
     top: -100,
-    
+
     right: 25,
     backgroundColor: "#007AFF",
     borderRadius: 40,
     paddingVertical: 15,
     paddingHorizontal: 15,
+  },
+  slide:{
+    
+    justifyContent:'center'
+  },
+  image: {
+
+    width:100,
+    height:100,
+    borderRadius:30
   },
   buttonText: {
     color: "white",
