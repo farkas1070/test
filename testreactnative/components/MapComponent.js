52db2ce468d33d5e7475bc25943b8860b5633f2a
Dávid Borsodi <corten.mgs@gmail.com>
2023-06-13 08:59:28 +0200

    push

--- testreactnative/components/MapComponent.js
+++ testreactnative/components/MapComponent.js
@@ -1,32 +1,21 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
-import Mapbox from '@rnmapbox/maps';
 
-Mapbox.setAccessToken('pk.eyJ1IjoiZmFya2FzMTA3MCIsImEiOiJjbGlzb2p6cTgwZ25wM25vN21ybzRlcjN3In0.7JbOZgr7nNADoWOwNsbWww');
-
-const App = () => {
+export default function App() {
   return (
-    <View style={styles.page}>
-      <View style={styles.container}>
-        <Mapbox.MapView style={styles.map} />
-      </View>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
 
-export default App;
-
 const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
   container: {
-    height: 300,
-    width: 300,
+    flex: 1,
   },
   map: {
-    flex: 1
-  }
+    width: '100%',
+    height: '100%',
+  },
 });
