2860962604fcd3c051545f0aa6d941b2e10ef01f
Dávid Borsodi <corten.mgs@gmail.com>
2023-06-13 09:22:18 +0200

    push

--- testreactnative/components/MapComponent.js
+++ testreactnative/components/MapComponent.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React,{useState}from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 
 export default function App() {
+
+  const [initialRegionValue, setInitialRegionValue] = useState({
+    latitude: 47.6803625,
+    longitude: 16.5721739,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+    
+});
+
   return (
     <View style={styles.container}>
       <MapView 
-      initialRegion={{
-        latitude: 47.6803625,
-        longitude: 16.5721739,
-        latitudeDelta: 10,
-        longitudeDelta: 10,
-    }}
+      initialRegion={initialRegionValue}
       style={styles.map} />
     </View>
   );
