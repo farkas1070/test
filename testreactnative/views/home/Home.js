830dc61ce0101e52d21d302594b7b33ebf026c1a
Dávid Borsodi <smarton0614@gmail.com>
2023-06-15 15:27:38 +0200

    push

--- testreactnative/views/home/Home.js
+++ testreactnative/views/home/Home.js
@@ -1,31 +1,11 @@
 import {  SafeAreaView, Text, View } from 'react-native'
-import React,{useEffect} from 'react'
+import React from 'react'
 import { styles } from "./HomeStyle";
 import Header from "./components/Header"
-import messaging from '@react-native-firebase/messaging';
-const Home = () => {
-  const requestUserPermission = async ()=>{
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-  
-    if (enabled) {
-      console.log('Authorization status:', authStatus);
-    } else {
-      console.log('failed to get token',authStatus)
-    }
-  }
-  useEffect(()=>{
 
-    if (requestUserPermission()) {
-      //return token from device
-      messaging().getToken().then(token =>{
-        console.log(token)
-      })
-    }
-  }, [])
- 
+const Home = () => {
+  
+
   
   return (
     <View style={styles.maincontainer}>
