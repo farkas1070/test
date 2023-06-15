6217ef039fdb660f446c963a6f1e55c1d1dd637f
Dávid Borsodi <smarton0614@gmail.com>
2023-06-15 08:53:09 +0200

    folders style

---
+++ testreactnative/views/home/HomeStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeStyle = () => {
+  return (
+    <View>
+      <Text>HomeStyle</Text>
+    </View>
+  )
+}
+
+export default HomeStyle
+
+const styles = StyleSheet.create({})
---
+++ testreactnative/views/map/MapStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MapStyle = () => {
+  return (
+    <View>
+      <Text>MapStyle</Text>
+    </View>
+  )
+}
+
+export default MapStyle
+
+const styles = StyleSheet.create({})
