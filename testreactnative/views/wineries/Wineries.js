5ca90db3c401768625224a872821002e26a991a4
hbela <hbela2000@gmail.com>
2023-06-16 10:06:10 +0200

    update

--- testreactnative/views/wineries/Wineries.js
+++ testreactnative/views/wineries/Wineries.js
@@ -1,17 +1,16 @@
 import { Text, View, ScrollView, Image } from "react-native";
-import React, { useEffect, useState,useContext } from "react";
-
+import React, { useEffect, useState, useContext } from "react";
 import { styles } from "./WineriesStyle";
 import Card from "./components/Card";
-import { pointsOfInterestContext } from "../../context/PointOfInterestContext.js"
-const Wineries = () => {
-  const [pointsOfInterest, setpointsOfInterest] = useContext(pointsOfInterestContext);
+import { pointsOfInterestContext } from "../../context/PointOfInterestContext.js";
 
-  
+const Wineries = () => {
+  const [pointsOfInterest, setpointsOfInterest] = useContext(
+    pointsOfInterestContext
+  );
 
   return (
     <View style={styles.maincontainer}>
-      
       <ScrollView style={styles.scrollview}>
         {pointsOfInterest.map((item, index) => {
           return <Card item={item} index={index} />;
