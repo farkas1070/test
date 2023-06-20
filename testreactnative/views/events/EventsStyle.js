c186b26226c302d2ed72c53a236dc2c9ecabc25d
hbela <hbela2000@gmail.com>
2023-06-20 11:40:25 +0200

    update

--- testreactnative/views/events/EventsStyle.js
+++ testreactnative/views/events/EventsStyle.js
@@ -1,76 +1,79 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet} from "react-native";
 export const styles = StyleSheet.create({
-  maincontainer: {
-    flex: 1,
-    backgroundColor: "white",
-    width: "100%",
-    height: "100%",
-  },
-  singlenews: {
-    height: 200,
-    backgroundColor: "beige",
-    margin: 10,
-    borderRadius: 20,
-    elevation: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  innerview: {
-    backgroundColor: "rgba(255,255,255,0.7)",
-    width: "50%",
-    height: "30%",
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 10,
-  },
-  text: {
-    textAlign: "center",
-  },
-  textinput: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    width: "70%",
-    borderRadius: 20,
-    backgroundColor: "white",
-  },
-  item: {
-    backgroundColor: "white",
-    flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight: 10,
-    marginTop: 17,
-  },
-  card: {
-    width: "100%",
-    height: 150,
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    marginBottom: 16,
-    elevation: 2,
-  },
-  itemText: {
-    fontSize: 18,
-    color: "black",
-    fontWeight: "bold",
-  },
-  image: {
-    width: 200,
-    height: 100,
-    borderRadius: 8,
-  },
-  dateText: {
-    fontSize: 12,
-    color: "black",
-  },
-  textCointainer: {
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+    maincontainer: {
+      flex: 1,
+      backgroundColor:'white',
+      width:'100%',
+      height:'100%',
+    },
+    singlenews:{
+      
+      height:200,
+      backgroundColor:'beige',
+      margin:10,
+      borderRadius:20,
+      elevation: 2,
+      justifyContent:'center',
+      alignItems: 'center',
+    },
+    innerview:{
+      backgroundColor:'rgba(255,255,255,0.7)',
+      width:'50%',
+      height:'30%',
+      borderRadius:20,
+      justifyContent: 'center',
+      alignItems:'center',
+      padding:10
+    },
+    text:{
+      textAlign: 'center',
+    },
+    textinput: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+      width: "70%",
+      borderRadius: 20,
+      backgroundColor: "white",
+    },
+    item: {
+      backgroundColor: "white",
+      flex: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      marginTop: 17,
+    },
+    card: {
+      width: "100%",
+      height: 150,
+      justifyContent: "space-between",
+      alignItems: "center",
+      backgroundColor: "#fff",
+      borderRadius: 8,
+      marginBottom: 16,
+      elevation: 2,
+    },
+    itemText: {
+      fontSize: 18,
+      color: "black",
+      fontWeight: "bold",
+    },
+    image: {
+      width: 200,
+      height: 100,
+      borderRadius: 8,
+    },
+    dateText: {
+      fontSize: 12,
+      color: "black",
+
+    },
+    textCointainer: {
+      textAlign: "center",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+
+  });
--- testreactnative/views/winery/Winery.js
+++ testreactnative/views/winery/Winery.js
@@ -2,13 +2,18 @@ import { ScrollView, Text, TextInput, Image } from "react-native";
 import React from "react";
 import { styles } from "./WineryStyle";
 import Placeholder from "../../assets/placeholder.png";
+<<<<<<< Updated upstream
 import RenderHtml from 'react-native-render-html';
 import { useWindowDimensions } from 'react-native';
 import {tagsStyles} from "./ContentStyle"
+=======
+import RenderHtml from "react-native-render-html";
+import { useWindowDimensions } from "react-native";
+>>>>>>> Stashed changes
 const Winery = ({ route }) => {
   const { width } = useWindowDimensions();
   const source = {
-    html: route.params.item.content
+    html: route.params.item.content,
   };
 
   return (
@@ -20,12 +25,16 @@ const Winery = ({ route }) => {
         }
       />
       <Text>{route.params.item.title}</Text>
+<<<<<<< Updated upstream
       
       <RenderHtml
       contentWidth={width}
       source={source}
       tagsStyles={tagsStyles}
     />
+=======
+      <RenderHtml contentWidth={width} source={source} />
+>>>>>>> Stashed changes
     </ScrollView>
   );
 };
