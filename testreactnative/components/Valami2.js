a99d15b0fd1e6a56e3796890eb6b61e2759193c6
Dávid Borsodi <smarton0614@gmail.com>
2023-06-27 11:55:37 +0200

    PUSH

--- testreactnative/components/Valami2.js
+++ testreactnative/components/Valami2.js
@@ -1,18 +1,34 @@
-import { StyleSheet, Text, View,Alert } from "react-native";
-import React, { useEffect,useState } from "react";
-import messaging from '@react-native-firebase/messaging';
+import { StyleSheet, Text, View, Alert,Switch ,PermissionsAndroid } from "react-native";
+import React, { useEffect, useState } from "react";
+import messaging from "@react-native-firebase/messaging";
 
 const Valami2 = () => {
-  
-  const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-    
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  const [notificationEnabled, setNotificationEnabled] = useState(true);
 
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
+  
+
+  const requestUserPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "Notification Permission",
+          message: "This app requires notification permission.",
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK",
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Notification permission granted.");
+        return true;
+      } else {
+        console.log("Notification permission denied.");
+        return false;
+      }
+    } catch (err) {
+      console.warn(err);
+      return false;
     }
   };
 
@@ -20,18 +36,23 @@ const Valami2 = () => {
     try {
       await messaging().registerDeviceForRemoteMessages();
       const token = await messaging().getToken();
-      console.log('FCM token:', token);
+      console.log("FCM token:", token);
     } catch (error) {
-      console.log('Failed to register for remote messages:', error);
+      console.log("Failed to register for remote messages:", error);
     }
   };
 
+
   useEffect(() => {
-    if (requestUserPermission()) {
-      //return fcm token for the device
-      registerForRemoteMessages()
-    } else {
-      console.log("failed token status", authStatus);
+    
+    if (notificationEnabled) {
+      if (requestUserPermission()) {
+        //return fcm token for the device
+        registerForRemoteMessages();
+        messaging().subscribeToTopic("all").then(()=>console.log("subscribed to topic"))
+      } else {
+        console.log("failed token status", authStatus);
+      }
     }
 
     // Register background handler
@@ -42,7 +63,7 @@ const Valami2 = () => {
     // Check whether an initial notification is available
     messaging()
       .getInitialNotification()
-      .then( async (remoteMessage) => {
+      .then(async (remoteMessage) => {
         if (remoteMessage) {
           console.log(
             "Notification caused app to open from quit state:",
@@ -60,20 +81,20 @@ const Valami2 = () => {
       );
     });
 
-    
-
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
     });
 
     return unsubscribe;
-
-
-  }, []);
+  }, [notificationEnabled]);
 
   return (
     <View>
       <Text>FCM tutorial</Text>
+      <Switch
+        value={notificationEnabled}
+        onValueChange={()=>{setNotificationEnabled(!notificationEnabled)}}
+      />
     </View>
   );
 };
