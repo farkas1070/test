9e9f9b22b6bebc761dbe4cc41c78ea79dcf902c2
Dávid Borsodi <smarton0614@gmail.com>
2023-07-04 08:44:45 +0200

    push

--- testreactnative/views/home/components/OnBoarding.js
+++ testreactnative/views/home/components/OnBoarding.js
@@ -1,9 +1,10 @@
-import { Image } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { Text, View } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 import { FontAwesome } from "@expo/vector-icons";
 import { Picker } from "@react-native-picker/picker";
+import { styles } from "./OnboardingStyle";
 
 const OnBoarding = () => {
   const [selectedLanguage, setSelectedLanguage] = useState();
@@ -12,11 +13,24 @@ const OnBoarding = () => {
       onDone={() => console.log("done")}
       pages={[
         {
+          backgroundColor: "#98FB98",
+          image: (
+            <Image
+              source={{
+                uri: "https://cdn.shopify.com/s/files/1/0525/4361/9238/files/3_f04ac13d-e77b-4f56-a04c-b16a901f18f4_1200x1200.png?v=1620164482",
+              }}
+              style={{ width: 200, height: 200 }}
+            />
+          ),
+          title: "Üdvözöl A Soproni Borvidék App",
+          subtitle: "Kezdjünk is Bele!",
+        },
+        {
           backgroundColor: "#fff",
           image: <FontAwesome name="language" size={132} color="black" />,
           title: "Válassz nyelvet",
           subtitle: (
-            <View>
+            <View style={{ width: "100%" }}>
               <Picker
                 selectedValue={selectedLanguage}
                 onValueChange={(itemValue, itemIndex) =>
@@ -31,30 +45,27 @@ const OnBoarding = () => {
         },
         {
           backgroundColor: "#fe6e58",
-          image: (
-            <Image
-              source={{
-                uri: "https://www.freepnglogos.com/uploads/circle-png/black-circle-thin-icon-download-9.png",
-              }}
-              style={{ width: 100, height: 100 }}
-            />
+          image: <FontAwesome name="bell-o" size={130} color="white" />,
+          title: "Engedélyek Megadása",
+
+          subtitle: (
+            <View style={{ width: "100%",marginTop:20 }}>
+              <Text style={styles.requestpermissiontext}>
+                Szükségünk van az alkalmazás megfelelő működéséhez a helyadatok
+                és értesítések engedélyezéséhez
+              </Text>
+              <View style={styles.buttoncontainer}>
+                <TouchableOpacity style={styles.notenablebutton}>
+                  <Text>Tiltás</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.enablebutton} >
+                  <Text>Engedélyezés</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
           ),
-          title: "The Title",
-          subtitle: "This is the subtitle that sumplements the title.",
         },
-        {
-          backgroundColor: "#999",
-          image: (
-            <Image
-              source={{
-                uri: "https://www.freepnglogos.com/uploads/circle-png/black-circle-thin-icon-download-9.png",
-              }}
-              style={{ width: 100, height: 100 }}
-            />
-          ),
-          title: "Triangle",
-          subtitle: "Beautiful, isn't it?",
-        },
+        
       ]}
     />
   );
---
+++ testreactnative/views/home/components/OnboardingStyle.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+
+export const styles = StyleSheet.create({
+    buttoncontainer:{
+        width:'100%',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:50,
+
+
+    },
+    notenablebutton:{
+        backgroundColor:'#EE4B2B',
+        width:120,
+        padding:10,
+        borderRadius:20,
+        alignItems:'center',
+        justifyContent:'center',
+
+
+    },
+    enablebutton:{
+        backgroundColor:'#3F704D',
+        padding:10,
+        width:120,
+        borderRadius:20,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    requestpermissiontext:{
+        textAlign:'center',
+        color:'white'
+    }
+})
